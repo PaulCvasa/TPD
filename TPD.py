0c947cbfbe1e5c02979e7e96963f79b52e25069b
Paul Cvasa <73744641+PaulCvasa@users.noreply.github.com>
2022-06-12 13:05:16 +0300

    added a simple GUI + some code cleanup

--- TPD.py
+++ TPD.py
@@ -1,17 +1,15 @@
 import time
 import numpy as np
 import os
-import urllib3
-import tarfile
+
 os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
 os.add_dll_directory("C:/Program Files/NVIDIA/CUDNN/v8.1/bin")
 import tensorflow as tf
 import cv2
+import PySimpleGUI as psg
 # Object detection imports from the module
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
-
-videoInput = cv2.VideoCapture('testRecs/tpd2.mp4')
 
 
 def modelSetup():
@@ -22,10 +20,7 @@ def modelSetup():
     #
     # By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
 
-    # What model to download.
     MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-    MODEL_FILE = MODEL_NAME + '.tar.gz'
-    DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
     # Path to frozen detection graph. This is the actual model that is used for the object detection.
     PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
@@ -33,16 +28,7 @@ def modelSetup():
     # List of the strings that is used to add correct label for each box.
     PATH_TO_LABELS = os.path.join('tensorflow/models/research/object_detection/data', 'mscoco_label_map.pbtxt')
 
-    NUM_CLASSES = 90
-
-    # ## Download Model
-    opener = urllib3.PoolManager()
-    opener.request('GET', DOWNLOAD_BASE + MODEL_FILE)
-    tar_file = tarfile.open(MODEL_FILE)
-    for file in tar_file.getmembers():
-        file_name = os.path.basename(file.name)
-        if 'frozen_inference_graph.pb' in file_name:
-            tar_file.extract(file, os.getcwd())
+    NUM_CLASSES = 8
 
     # ## Load a (frozen) Tensorflow model into memory.
     detection_graph = tf.Graph()
@@ -58,7 +44,8 @@ def modelSetup():
     # we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a
     # dictionary mapping integers to appropriate string labels would be fine
     label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                                use_display_name=True)
     category_index = label_map_util.create_category_index(categories)
     return detection_graph, category_index
 
@@ -99,7 +86,8 @@ def detection(input):
                     np.squeeze(scores),
                     category_index,
                     use_normalized_coordinates=True,
-                    line_thickness=8)
+                    min_score_thresh=.35,
+                    line_thickness=4)
                 # Compute distances to TPs and send warnings
                 for i, b in enumerate(boxes[0]):
                     if classes[0][i] == 1 or classes[0][i] == 3 or classes[0][i] == 6 or classes[0][i] == 8:  # if it's a car, a truck or a bus
@@ -118,10 +106,44 @@ def detection(input):
                                                 cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
 
                 last_time = time.time()
-                cv2.imshow('Traffic Participants Detection', cv2.resize(frame, (800, 450)))
+                cv2.imshow('Traffic Participants Detection - Press Q to stop the detection',
+                           cv2.resize(frame, (800, 450)))
+
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     cv2.destroyAllWindows()
                     break
 
 
-detection(videoInput)
+def main():
+    videoInput = cv2.VideoCapture(0)  # 'testRecs/tpd2.mp4'
+    testInput = cv2.VideoCapture('testRecs/tpd1.mp4')
+
+    psg.theme("Dark")
+    buttonsLine = [
+        [psg.Button("START DETECTION", size=(20, 10)),
+         psg.Button("TEST DETECTION", size=(20, 10), button_color='Blue'),
+         psg.Button("SETTINGS", size=(20, 10), button_color='Gray'),
+         psg.Button("EXIT", size=(20, 10), button_color='Red')]
+    ]
+    # Layout window config
+    layout = [
+        [psg.Text("Traffic Participants Detection", size=(120, 40), justification="center")],
+        [psg.Image(filename="", key="-IMAGE-")],
+        [psg.Column(buttonsLine, vertical_alignment='center', justification='center', k='-C-')]
+    ]
+
+    window = psg.Window("TPD", layout, location=(800, 400), resizable=True, finalize=True)
+    while True:
+        event, values = window.read(timeout=20)
+        if event == "START DETECTION":
+            detection(videoInput)
+        if event == "TEST DETECTION":
+            detection(testInput)
+        if event == "SETTINGS":
+            # todo
+            break
+        if event == "EXIT" or event == psg.WIN_CLOSED:
+            break
+
+
+main()
