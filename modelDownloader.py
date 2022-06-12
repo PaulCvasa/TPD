ea13d44d92a346f2d7c5072fd68d86dceb92515f
Paul Cvasa <73744641+PaulCvasa@users.noreply.github.com>
2022-06-12 13:28:36 +0300

    added the model downloader

---
+++ modelDownloader.py
@@ -0,0 +1,16 @@
+import urllib3
+import tarfile
+import os
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# ## Download Model
+opener = urllib3.PoolManager()
+opener.request('GET', DOWNLOAD_BASE + MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+    file_name = os.path.basename(file.name)
+    if 'frozen_inference_graph.pb' in file_name:
+        tar_file.extract(file, os.getcwd())
